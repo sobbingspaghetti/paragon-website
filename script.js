2db79f152a2647ac13e912d14dda7eb5d99e734f
sobbingspaghetti <bcflanders14@gmail.com>
2025-02-18 14:34:16 -0500

    new upload

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
 
-//form
 
-document.getElementById('tv-input').addEventListener('submit', async function(event) {
-    event.preventDefault();  // Prevents the form from refreshing the page
-    
-    const keywordInput = document.getElementById('keywordInput');  // Get the input element
-    const secretWord = keywordInput.value;  // Get the value of the input
-    const errorSound = document.getElementById('error-sound'); // Get the audio element
-
-    const response = await fetch('http://localhost:3000/check-secret', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ secret_word: secretWord })
-    });
-
-    const result = await response.json();  // Get the JSON response from the backend
-
-    
-    keywordInput.placeholder = result.message;  // Replace placeholder text with response
-    keywordInput.value = '';  // Clear the input field
-});
-
-
-
-//music
+// MUSIC //
 
 const songs = [
     { url: "audio/bluevelvet.mp3", title: "Blue Velvet", artist: "Bobby Vinton" },
@@ -91,3 +68,12 @@ function showPopup(title, artist) {
         }, fadeTime * 1000);
     }, scrollTime * 1000);
 }
+
+
+
+
+//  entersubmit //
+
+//KEYWORDS//
+
+
